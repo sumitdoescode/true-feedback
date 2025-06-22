3a78a1b55dd6764da169508151d22aa8dc9eaed3
Sumit Raj <90326704+sumitdoescode@users.noreply.github.com>
2025-06-22 20:11:42 +0530

    added a comment in layout.jsx file
    please don't take this seriously

--- app/layout.jsx
+++ app/layout.jsx
@@ -16,6 +16,7 @@ export const metadata = {
     description: "Real feedback from real people.",
 };
 
+// this is a very faltu comment, will remove this later
 export default function RootLayout({ children }) {
     return (
         <ClerkProvider
