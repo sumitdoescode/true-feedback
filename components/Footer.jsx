83e30250b96aa51f3f10bdd331a4e163b725bc29
Sumit Raj <sumit.does.code@gmail.com>
2025-10-05 05:44:41 +0530

    hey there

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
         <div className="">
             <div className="container mx-auto px-2">
                 <p className="text-base md:text-xl text-center py-2">
-                    Developed by{" "}
+                    Developed by
                     <Link href="https://sumitdoescode.vercel.app/" target="_blank" className="text-primary bg-neutral-950 px-2">
                         sumitdoescode
                     </Link>
