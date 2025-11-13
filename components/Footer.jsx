8b7c8e30212ee57011241a605f0c8902d6252fa2
Sumit Raj <sumit.does.code@gmail.com>
2025-11-13 18:56:13 +0530

    change font size

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
     return (
         <div className="">
             <div className="container mx-auto px-2">
-                <p className="text-sm md:text-xl text-center py-2">
+                <p className="text-base text-center py-2">
                     Design & Developed by
                     <Link href="https://sumitdoescode.vercel.app/" target="_blank" className="text-primary bg-neutral-950 px-1 underline">
                         sumitdoescode
