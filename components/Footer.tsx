d33620d432b905301ae3aeea17e7df6acb8baa90
Sumit Raj <sumit.does.code@gmail.com>
2026-01-11 19:33:50 +0530

    more changes

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,19 +1,20 @@
 import React from "react";
 import Link from "next/link";
+import Container from "./Container";
 
 const Footer = () => {
-   return (
-      <div className="">
-         <div className="container mx-auto px-2">
-            <p className="text-base text-center py-2">
-               Design & Developed by
-               <Link href="https://sumitdoescode.vercel.app/" target="_blank" className="text-primary bg-neutral-950 px-1 underline">
-                  sumitdoescode
-               </Link>
-            </p>
-         </div>
-      </div>
-   );
+    return (
+        <div className="bg-neutral-900 py-2">
+            <Container>
+                <p className="text-sm text-center">
+                    Design & Developed by
+                    <Link href="https://sumitdoescode.vercel.app/" target="_blank" className="text-primary bg-neutral-950 px-1 underline">
+                        sumitdoescode
+                    </Link>
+                </p>
+            </Container>
+        </div>
+    );
 };
 
 export default Footer;
