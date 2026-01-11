8ed552aa0b3abcc1e43bc406f9f5113136d9c5da
Sumit Raj <sumit.does.code@gmail.com>
2026-01-11 20:18:17 +0530

    comment

--- schemas/auth.schema.ts
+++ schemas/auth.schema.ts
@@ -1,11 +1,13 @@
 import { z } from "zod";
 
 export const CompleteProfileSchema = z.object({
-   username: z
-      .string()
-      .regex(/^(?![._])(?!.*[._]{2})[a-z._]+(?<![._])$/, "Username can contain lowercase letters, dots and underscores only")
-      .min(3, "Username must be at least 3 characters long")
-      .max(16, "Username must be at most 16 characters long"),
+    username: z
+        .string()
+        .regex(/^(?![._])(?!.*[._]{2})[a-z._]+(?<![._])$/, "Username can contain lowercase letters, dots and underscores only")
+        .min(3, "Username must be at least 3 characters long")
+        .max(16, "Username must be at most 16 characters long"),
 });
+
+// working ?
 
 export type CompleteProfileType = z.infer<typeof CompleteProfileSchema>;
