8460fabd3b6e5c51e55ef13c617c5ed73310f6aa
sumitdoescode <sumit.does.code@gmail.com>
2026-02-02 23:56:19 +0530

    u know

--- app/(main)/dashboard/page.tsx
+++ app/(main)/dashboard/page.tsx
@@ -15,8 +15,6 @@ const page = async () => {
         headers: await headers(),
     });
 
-    console.log(session);
-
     // if (!session) {
     //     redirect("/login");
     // }
--- lib/auth.ts
+++ lib/auth.ts
@@ -1,7 +1,8 @@
 import { betterAuth } from "better-auth";
 import { MongoClient } from "mongodb";
 import { mongodbAdapter } from "better-auth/adapters/mongodb";
-import { username } from "better-auth/plugins";
+import { createAuthMiddleware, username } from "better-auth/plugins";
+import { ObjectId } from "mongodb";
 
 const client = new MongoClient(process.env.MONGODB_URI!);
 const db = client.db("true-feedback-dev");
@@ -9,9 +10,11 @@ const db = client.db("true-feedback-dev");
 export const auth = betterAuth({
     baseURL: process.env.BETTER_AUTH_URL!,
     database: mongodbAdapter(db),
+
     emailAndPassword: {
         enabled: true,
     },
+
     user: {
         additionalFields: {
             isAcceptingMessages: {
@@ -26,9 +29,27 @@ export const auth = betterAuth({
             maxUsernameLength: 20,
         }),
     ],
-    // hooks : {
-    //     after :
-    // },
+    hooks: {
+        after: createAuthMiddleware(async (ctx) => {
+            const newUser = ctx.context.newSession?.user;
+
+            if (!newUser) return;
+            const userDoc = await db.collection("user").findOne({ email: newUser.email });
+            if (userDoc?.username) return;
+
+            const generatedUsername = newUser.email.split("@")[0];
+            console.log(generatedUsername);
+            await db.collection("user").updateOne(
+                { _id: new ObjectId(newUser.id) },
+                {
+                    $set: {
+                        username: generatedUsername,
+                        displayUsername: generatedUsername,
+                    },
+                },
+            );
+        }),
+    },
     socialProviders: {
         google: {
             clientId: process.env.GOOGLE_CLIENT_ID!,
