5575e9c2078736ac4f34074ffe32aacaca0f2ca0
sumitdoescode <sumit.does.code@gmail.com>
2026-05-28 21:37:21 +0530

    feat: implement message route for AI question generation and user messaging

--- app/api/message/route.ts
+++ app/api/message/route.ts
@@ -11,61 +11,61 @@ const ai = new GoogleGenAI({ apiKey: process.env.GEMINI_API_KEY });
 // GET => /api/message
 // this route is used to generate a random question
 export async function GET() {
-    try {
-        const response = await ai.models.generateContent({
-            model: `${process.env.AI_MODEL_NAME}`,
-            contents:
-                "Generate a open-ended and engaging question as a single string. The question is for an anonymous social messaging platform, like Qooh.me, and should be suitable for a diverse audience. Focus should be on universal themes that encourage good and positive interaction. For example, your output should be structured like this: 'What's a hobby you've recently started?'. Make sure to ask only one question and also make sure language is easy. Ensure the questions are bit complex, foster curiosity, and contribute to a positive and non-toxic conversational environment. Make sure the question is short and concise.",
-        });
+  try {
+    const response = await ai.models.generateContent({
+      model: `${process.env.AI_MODEL_NAME}`,
+      contents:
+        "Generate a open-ended and engaging question as a single string. The question is for an anonymous social messaging platform, like Qooh.me, and should be suitable for a diverse audience. Focus should be on universal themes that encourage good and positive interaction. For example, your output should be structured like this: 'What's a hobby you've recently started?'. Make sure to ask only one question and also make sure language is easy. Ensure the questions are bit complex, foster curiosity, and contribute to a positive and non-toxic conversational environment. Make sure the question is short and concise.",
+    });
 
-        if (!response.text) {
-            return NextResponse.json({ success: false, data: "No response from AI" }, { status: 500 });
-        }
-
-        return NextResponse.json({ success: true, content: response.text });
-    } catch (error: unknown) {
-        console.error("Error generating message:", error);
-        return NextResponse.json({ success: false, data: error instanceof Error ? error.message : "Internal Server Error" }, { status: 500 });
+    if (!response.text) {
+      return NextResponse.json({ success: false, data: "No response from AI" }, { status: 500 });
     }
+
+    return NextResponse.json({ success: true, content: response.text });
+  } catch (error: unknown) {
+    console.error("Error generating message:", error);
+    return NextResponse.json({ success: false, data: error instanceof Error ? error.message : "Internal Server Error" }, { status: 500 });
+  }
 }
 
 // POST => /api/message
 // this route is used to send a message
 export async function POST(request: NextRequest) {
-    try {
-        await connectDB();
+  try {
+    await connectDB();
 
-        const data = await request.json();
-        const result = SendMessageSchema.safeParse(data);
-        if (!result.success) {
-            return NextResponse.json({ success: false, error: flattenError(result.error).fieldErrors }, { status: 400 });
-        }
-        const { content, to } = result.data;
-        const username = to.trim().toLowerCase();
-
-        // direct db query
-        const user = await db.collection("user").findOne(
-            { username },
-            {
-                projection: {
-                    id: 1,
-                    isAcceptingMessages: 1,
-                },
-            },
-        );
-
-        if (!user) {
-            return NextResponse.json({ success: false, message: "User not found" }, { status: 404 });
-        }
-
-        if (!user.isAcceptingMessages) {
-            return NextResponse.json({ success: false, message: "This user is not accepting messages" }, { status: 403 });
-        }
-
-        await Message.create({ content, receiver: user._id });
-
-        return NextResponse.json({ success: true, message: "Message sent successfully" }, { status: 200 });
-    } catch (error) {
-        return NextResponse.json({ success: false, message: error instanceof Error ? error.message : "Internal Server Error" }, { status: 500 });
+    const data = await request.json();
+    const result = SendMessageSchema.safeParse(data);
+    if (!result.success) {
+      return NextResponse.json({ success: false, error: flattenError(result.error).fieldErrors }, { status: 400 });
     }
+    const { content, to } = result.data;
+    const username = to.trim().toLowerCase();
+
+    // direct db query
+    const user = await db.collection("user").findOne(
+      { username },
+      {
+        projection: {
+          id: 1,
+          isAcceptingMessages: 1,
+        },
+      },
+    );
+
+    if (!user) {
+      return NextResponse.json({ success: false, message: `User not found with username ${username}` }, { status: 404 });
+    }
+
+    if (!user.isAcceptingMessages) {
+      return NextResponse.json({ success: false, message: `This user ${username} is not accepting messages` }, { status: 403 });
+    }
+
+    await Message.create({ content, receiver: user._id });
+
+    return NextResponse.json({ success: true, message: "Message sent successfully" }, { status: 200 });
+  } catch (error) {
+    return NextResponse.json({ success: false, message: error instanceof Error ? error.message : "Failed to send Message" }, { status: 500 });
+  }
 }
